a237274fc0438742283ac1e931ba305477ca0744
xLisu <pocztalis@icloud.com>
2017-01-20 23:59:26 +0100

    Added End

--- code/End.js
+++ code/End.js
@@ -1,13 +1,23 @@
 var glState = glState||{};
 
+
+
 glState.Game = class {
 	constructor() {
         this.endtext;
+        
     }
     
     create() {
         this.add.sprite(0,0,'endscreen');
-        this.end.
+        this.game.add.text(280, 145, "You got Pop'ed.");
+        this.endtext = this.game.add.text(290,160, "SCORE:" + this.score);
+        //Highest score
+        if (this.score > this.highestscore) {
+            this.highestscore = this.score
+            this.game.add.text(400, 145, "You ")
+        };
+        
         
     }
     
