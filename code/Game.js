88e88e2fb219b1c1b85be0c9463060bade4d780d
makiki99 <mat.gawro@gmail.com>
2017-01-20 21:31:09 +0100

    broken waves

--- code/Game.js
+++ code/Game.js
@@ -28,13 +28,13 @@ glState.Game = class {
 
 		//Punkty/Timer
 		this.timer = game.time.create(false);
-	this.timer.loop(100, this.counttime, this);
-	this.timer.start();
+		this.timer.loop(100, this.counttime, this);
+		this.timer.start();
 
-	//Display score
-	this.displayscore = this.game.add.text(500, 500, "", { fill:"#ffffff"} );
-
-	this.displayscore.text = this.score;
+		//Display score
+		this.displayscore = this.game.add.text(500, 500, "", { fill:"#ffffff"} );
+		this.displayscore.font = 'monospace';
+		this.displayscore.text = this.score;
 
 		this.bullets = this.add.group();
 		for (let i = 0; i < 2000; i++) {
@@ -58,18 +58,30 @@ glState.Game = class {
 		this.bounds[3].body.setSize(800,400,0,512); //bottom
 	}
 	update() {
-		//temp
-		// this.shoot(this.test, {
-		// 	x: 0,
-		// 	y: 0,
-		// 	vx: Math.random()*200-100,
-		// 	vy: Math.random()*600-300,
-		// 	gy: 100
-		// });
+		// temp
+		this.shoot(this.test, {
+			x: 100,
+			y: 100,
+			vx: 100,
+			vy: 100,
+			ay: 50,
+			absoluteX: true,
+			absoluteY: true,
+			waveY: true,
+		});
 		this.movePlayer();
 		this.bounds.forEach(i => {
 			game.physics.arcade.collide(this.player,i);
 			i.visible = false;
+		});
+		this.bullets.children.forEach(i => {
+			if (i.waveY) {
+				if (i.body.velocity.y > 0) {
+					i.body.acceleration.y = -Math.abs(i.body.acceleration.y);
+				} else if (i.body.velocity.y < 0) {
+					i.body.acceleration.y = Math.abs(i.body.acceleration.y);
+				}
+			}
 		});
 	}
 	movePlayer() {
@@ -100,11 +112,15 @@ glState.Game = class {
 		opts.y = (opts.y === undefined) ? 0 : opts.y;
 		opts.vx = (opts.vx === undefined) ? 0 : opts.vx;
 		opts.vy = (opts.vy === undefined) ? 0 : opts.vy;
+		opts.ax = (opts.ax === undefined) ? 0 : opts.ax;
+		opts.ay = (opts.ay === undefined) ? 0 : opts.ay;
 		opts.gx = (opts.gx === undefined) ? 0 : opts.gx;
 		opts.gy = (opts.gy === undefined) ? 0 : opts.gy;
 		opts.absoluteX = !!opts.absoluteX;
 		opts.absoluteY = !!opts.absoluteY;
 		opts.lifespan = (opts.lifespan === undefined) ? 0 : opts.lifespan;
+		opts.waveY = !!opts.waveY;
+		opts.waveX = !!opts.waveX;
 
 		if (!opts.absoluteX) {
 			opts.x += src.x;
@@ -117,16 +133,18 @@ glState.Game = class {
 		this.bullets.children[this.bulletPtr].lifespan = opts.lifespan;
 		this.bullets.children[this.bulletPtr].body.velocity.x = opts.vx;
 		this.bullets.children[this.bulletPtr].body.velocity.y = opts.vy;
+		this.bullets.children[this.bulletPtr].body.acceleration.x = opts.ax;
+		this.bullets.children[this.bulletPtr].body.acceleration.y = opts.ay;
 		this.bullets.children[this.bulletPtr].body.gravity.x = opts.gx;
 		this.bullets.children[this.bulletPtr].body.gravity.y = opts.gy;
+		this.bullets.children[this.bulletPtr].waveY = opts.waveY;
+		this.bullets.children[this.bulletPtr].waveX = opts.waveX;
 		this.bullets.children[this.bulletPtr].revive();
 		this.bulletPtr++;
 		if (this.bulletPtr >= this.bullets.length) {
 			this.bulletPtr = 0;
 		}
 	}
-
-
 	counttime() {
 		this.ms++;
 		this.score = this.ms * 100;
