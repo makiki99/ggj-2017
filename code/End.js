e2d51c44122e59f6afdc9e0cd5147715129481e4
xLisu <pocztalis@icloud.com>
2017-01-21 02:20:37 +0100

    end

--- code/End.js
+++ code/End.js
@@ -2,7 +2,7 @@ var glState = glState||{};
 
 
 
-glState.Game = class {
+glState.End = class {
 	constructor() {
         this.endtext;
         
@@ -11,18 +11,25 @@ glState.Game = class {
     create() {
         this.add.sprite(0,0,'endscreen');
         this.game.add.text(280, 145, "You got Pop'ed.");
-        this.endtext = this.game.add.text(290,160, "SCORE:" + this.score);
+        this.endtext = this.game.add.text(300,180, "SCORE: " + score);
         //Highest score
-        if (this.score > this.highestscore) {
-            this.highestscore = this.score
-            this.game.add.text(400, 145, "You ")
-        };
+        if (score > highestscore) {
+            highestscore = score
+            this.game.add.text(280, 350, "You've beaten your old highscore! :)");
+            this.game.add.text(400, 390, "Your current highscore is: " + highestscore);
+        } else {
+            this.game.add.text(280, 350, "Your highest score is: " + highestscore);
+        }
         
         
+        
+        this.game.add.text(50, 570, "Press enter to try again.");
+        this.enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+		this.enter.onDown.add(this.startMenu, this);
     }
+    startMenu(){
+        game.state.start("main")
+    }
+}
     
-    update() {
-        
-        
-        
-    }
+ 
